388445cb8a1a36716cf8c1e2cac49cdb6f3c8a98
Konsul117 <konsul117@bk.ru>
2016-03-14 23:31:07 +1000

    добавлены php-доки

--- components/ActiveFieldCustom.php
+++ components/ActiveFieldCustom.php
@@ -1,30 +1,37 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: konsul
- * Date: 14.03.16
- * Time: 22:58
- */
 
 namespace app\components;
 
-
 use yii\widgets\ActiveField;
 
+/**
+ * Расширение стандартного ActiveField для переопределения функций обёртки поля.
+ */
 class ActiveFieldCustom extends ActiveField {
 
+	/** @var bool Не оборачивать поле в div */
 	public $noWrap = false;
 
+	/**
+	 * @inheritdoc
+	 */
 	public function begin() {
+		//если нужно оборачивать
 		if (!$this->noWrap) {
+			//то вызываем родительскую функцию
 			return parent::begin();
 		}
 
 		return '';
 	}
 
+	/**
+	 * @inheritdoc
+	 */
 	public function end() {
+		//если нужно оборачивать
 		if (!$this->noWrap) {
+			//то вызываем родительскую функцию
 			return parent::begin();
 		}
 
