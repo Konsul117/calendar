8aea51ba4bcef3c9c280cdce139b83a4f54c1dd7
Konsul117 <konsul117@bk.ru>
2018-07-23 03:40:44 +1000

    пример локального конфига

---
+++ config/common-local.example.php
@@ -0,0 +1,20 @@
+<?php
+return [
+	'components' => [
+		'db'           => [
+			'dsn'      => 'mysql:host=localhost;dbname=',
+			'username' => '',
+			'password' => '',
+		],
+	],
+	'params'     => [
+		'proxies'                => [
+			[
+				'address' => 'address',
+				'port'    => 'port',
+			],
+		],
+		'telegramApiKey'         => 'token',
+		'telegramHookLogEnabled' => false,
+	],
+];
