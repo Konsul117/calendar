ae37a39d5c2ca0112a226c2c166bf0835fb252fa
Konsul117 <konsul117@bk.ru>
2021-03-22 04:05:00 +0300

    login redirect fix

--- views/user/login.php
+++ views/user/login.php
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?php $form = ActiveForm::begin([
         'id' => 'login-form',
-        'action' => Yii::getAlias('@web/user/login'),
+        'action' => Yii::getAlias('@web/user/login/'),
         'options' => ['class' => 'form-horizontal'],
         'fieldConfig' => [
             'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
